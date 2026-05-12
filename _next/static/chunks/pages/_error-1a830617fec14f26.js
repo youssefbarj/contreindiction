ff9d69460006d5ccc4b72ddffb230b8328deb311
youssefbarj <youssefbarj@users.noreply.github.com>
2026-05-12 13:18:58 +0000

    deploy: df7b68b36888e159f43da9bd14efab673a55b289

---
+++ _next/static/chunks/pages/_error-1a830617fec14f26.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{4519:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(5159)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(4519)),_N_E=_.O()}]);
